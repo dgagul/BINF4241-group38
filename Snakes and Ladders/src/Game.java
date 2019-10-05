e54846866615887886a0ce9e61af30b9a4125c95
lzumta <45519052+lzumta@users.noreply.github.com>
2019-10-05 22:17:41 +0200

    Created class Game which initializes the game(Squares, Players, Ladders and Snakes) keeps track of isfinished, winner)

--- Snakes and Ladders/src/Game.java
+++ Snakes and Ladders/src/Game.java
@@ -34,17 +34,17 @@ public class Game {
             player2.name = name2;
             player2.position = 1;
 
-            if(!name3.equals("None")){
-                playerqueue.add(name3);
-                Player player3 = new Player();
-                player3.name = name3;
-                player3.position = 1;}
+        if (name3.equals("None")){
+            playerqueue.add(name3);
+            Player player3 = new Player();
+            player3.name = name3;
+            player3.position = 1;}
 
-            if (!name4.equals("None")){
-                playerqueue.add(name4);
-                Player player4 = new Player();
-                player4.name = name2;
-                player4.position = 1;}
+        if (!name4.equals("None")){
+            playerqueue.add(name4);
+            Player player4 = new Player();
+            player4.name = name2;
+            player4.position = 1;}
 
             // Initializes Squares
             // sets isoccupied of firstsquare to true
@@ -53,16 +53,16 @@ public class Game {
             // this means that the constructor of the Square class has to set all
             // attributes to false initially
 
-            for (int i = 1; i<= boardsize; i++) {
-                Square square = new Squares();
-                square.number = i;
-                if (i == 1) {
-                    square.isoccupied = true;
-                    square.isstart = true;
-                }
+        for (int i = 1; i<= boardsize; i++) {
+            Square square = new Squares();
+            square.number = i;
+            if (i == 1) {
+                square.isoccupied = true;
+                square.isstart = true;
+            }
 
-                if (i == boardsize) {
-                    square.isend = true; }
+            if (i == boardsize) {
+                square.isend = true; }
             }
 
             // Set random Snadders (random if its a ladder or a snadder)
@@ -72,13 +72,13 @@ public class Game {
             // .end needs to be calculated in Snadder class
             // when it is a ladder make square.number + 2
             // when it is a snake make square.number - 2
-            for (int j=4; j < boardsize -4; j+= 5){
-                Snadder snadder = new Snadder();
-                snadder.start = j;
-                Random random = new Random();
-                ladderorsnadder = random.nextBoolean();
-                snadder.isladder = ladderorsnadder;
-                snadder.issnake = !ladderorsnadder;}
+        for (int j=4; j < boardsize -4; j+= 5){
+            Snadder snadder = new Snadder();
+            snadder.start = j;
+            Random random = new Random();
+            ladderorsnadder = random.nextBoolean();
+            snadder.isladder = ladderorsnadder;
+            snadder.issnake = !ladderorsnadder;}
 
         }
 
