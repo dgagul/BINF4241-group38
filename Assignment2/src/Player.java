c9de66902612238f74a9221975f315590c83f842
lzumta <45519052+lzumta@users.noreply.github.com>
2019-10-16 12:41:55 +0200

    added Player class

--- Assignment2/src/Player.java
+++ Assignment2/src/Player.java
@@ -1,4 +1,66 @@
-package PACKAGE_NAME;
+
+import java.util.ArrayList;
 
 public class Player {
+
+    private boolean iswhite;
+    private String name;
+    boolean c_iswhite;
+    String c_name;
+
+    private List<Piece> rest_pieces = new ArrayList<>();
+    private List<Piece> eaten_pieces = new ArrayList<>();
+
+
+    public Player(boolean whiteplayer, String playername) {
+        iswhite = whiteplayer;
+        name = playername;
+        c_iswhite = iswhite;
+        c_name = name; }
+
+        // getter just returning copys
+    public boolean getIswhite(){
+        return c_iswhite; }
+
+    public String getName(){
+        return c_name; }
+
+    public List<Piece> getRest_pieces() {
+        return new ArrayList<Pieces>(rest_pieces); }
+
+    public List<Piece> getEaten_pieces_pieces() {
+        return new ArrayList<Pieces>(rest_pieces);}
+    /*
+    private final int PAWNS = 8;
+    private final int BISHOPS = 2;
+    private final int ROOKS = 2;
+    public void initializePieces(){
+        if(this.white == true){
+            for(int i=0; i<PAWNS; i++){ // draw pawns
+                pieces.add(new Pawn(true,i,2));
+            }
+            pieces.add(new Rook(true, 0, 0));
+            pieces.add(new Rook(true, 7, 0));
+            pieces.add(new Bishop(true, 2, 0));
+            pieces.add(new Bishop(true, 5, 0));
+            pieces.add(new Knight(true, 1, 0));
+            pieces.add(new Knight(true, 6, 0));
+            pieces.add(new Queen(true, 3, 0));
+            pieces.add(new King(true, 4, 0));
+        }
+        else{
+            for(int i=0; i<PAWNS; i++){ // draw pawns
+                pieces.add(new Pawn(true,i,6));
+            }
+            pieces.add(new Rook(true, 0, 7));
+            pieces.add(new Rook(true, 7, 7));
+            pieces.add(new Bishop(true, 2, 7));
+            pieces.add(new Bishop(true, 5, 7));
+            pieces.add(new Knight(true, 1, 7));
+            pieces.add(new Knight(true, 6, 7));
+            pieces.add(new Queen(true, 3, 7));
+            pieces.add(new King(true, 4, 7));
+        }
+
+    }*/
 }
