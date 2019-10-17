8d8dba9c8da0cdc0f27dc038ecfd306179a5d23e
tsunama <marion@duebendorfer.ch>
2019-10-17 15:52:04 +0200

    add constructor

--- Assignment2/src/Game.java
+++ Assignment2/src/Game.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
-
 public class Game{
     private static boolean isFinished = false;
-    //private static Player currentPlayer = null;
+    private Player playerWhite;
+    private Player playerBlack;
+    private Board board;
 
+    public Game(){
+        playerWhite = new Player(Player.Color.WHITE);
+        playerBlack = new Player(Player.Color.BLACK);
+        board = new Board();
+    }
 
     public static void main(String args[]) {
-        ChessBoard chessBoard = new ChessBoard();
-        // Player player1 = new Player();
-        // Player player2 = new Player();
+        Game game = new Game();
 
 
         //currentPlayer = player1;
