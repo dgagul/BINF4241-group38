b85d2c36a43011e252bf7c05e5eb4544e34cc400
dgagul <dario.gagulic@uzh.ch>
2019-11-22 16:18:42 +0100

    latest version

--- Assignment4/src/RobotState.java
+++ Assignment4/src/RobotState.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
 public interface RobotState {
-    public void setTimer(int time);
-    public void start();
-    public void checkCleaning();
-    public void checkBattery();
-    public void checkCharging();
-    public void completeCleaning();
-    public void interrupt();
-    public ArrayList<Command> possibleCommands();
-    public void updateRobot(int timer, int battery, int charge, int completition);
+    void setTimer(int time);
+    void start();
+    void checkCleaning();
+    void checkBattery();
+    void checkCharging();
+    void completeCleaning();
+    void interrupt();
+    ArrayList<Command> possibleCommands();
+    void updateRobot(int timer, int battery, int charge, int completition);
 }
