3880d75001dabb00dde892d410620c0174c5a2e2
lzumta <45519052+lzumta@users.noreply.github.com>
2019-11-27 20:48:17 +0100

    created new folder for assignment 5 and copied code of assignment 1 snakes and ladders

---
+++ Assignment5/src/Snadder.java
@@ -0,0 +1,37 @@
+public class Snadder extends Square {
+
+    boolean isSnadder;
+
+    int end;
+
+
+
+    Snadder(int i, boolean isLadder) {
+
+        super(i);
+
+        isSnadder = true;
+
+        if (isLadder) {
+
+            end = i + 2;
+
+        } else {
+
+            end = i - 2;
+
+        }
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
