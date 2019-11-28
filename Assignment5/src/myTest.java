81038b64097b8c53cf22e6cc9d785a6a034ddb3f
lzumta <45519052+lzumta@users.noreply.github.com>
2019-11-28 17:06:11 +0100

    myTest Class

---
+++ Assignment5/src/myTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+
+public class myTest {
+
+    @Test
+    public void testEmptyBoardSizeInput(){
+        myTest newTest =  new myTest();
+        assertEquals(0, newTest.)
+
+
+    }
+}
